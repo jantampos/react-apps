524ce40c88429e1ea018d6bdbf106b2db03b640e
jantampos <janaubrey.tampos@gmail.com>
2023-07-20 11:55:36 +0100

    Third-party libraries in React

--- hacker-stories/src/AppNative.js
+++ hacker-stories/src/AppNative.js
@@ -1,7 +1,26 @@
 import { React, useState, useEffect, useRef, useReducer, useCallback } from 'react';
-import axios from 'axios';
 import './App.css';
 
+/* Dummy data
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+*/
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='; 
 
 const useSemiPersistentState = (key, initialState) => {
@@ -14,6 +33,28 @@ const useSemiPersistentState = (key, initialState) => {
   }, [value, initialState]); // should add 'key' to avoid the side-effect may run with an outdated key (also called stale) if the key changed between renders
   return [value, setValue]; 
 };
+
+/* Dummpy promise
+// const getAsyncStories = () =>
+//   new Promise((resolve, reject) => setTimeout(reject, 2000));
+
+const getAsyncStories = () => {
+  return new Promise(resolve => setTimeout(() => resolve({ data : {stories : initialStories }}), 2000));
+};
+*/
+
+/* Initial reducer implementation
+const storiesReducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(story => action.payload.objectID !== story.objectID);
+    default:
+      throw new Error();
+  }
+}
+*/
 
 const storiesReducer = (state, action) => {
   switch(action.type) {
@@ -50,25 +91,44 @@ const storiesReducer = (state, action) => {
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
   const [url , setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
+  /* Replacing useState with useReducer
+  const [stories, setStories] = useState([]); 
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  */
   const [stories, dispatchStories] = useReducer(
     storiesReducer, 
     { data: [], isLoading: false, isError: false }
   );
 
   const handleFetchStories = useCallback(() => {
+    //if (!searchTerm) return;
+    
+    /* setIsLoading(true); */
     dispatchStories({
       type: 'STORIES_FETCH_INIT',
     });
 
-    axios
-      .get(url)
+    /* replace dummy promise
+    getAsyncStories()
+    */
+
+    fetch(`${API_ENDPOINT}${searchTerm}`)
+      .then(response => response.json())
       .then(result => {
+        /*
+        setStories(result.data.stories); 
+        setIsLoading(false);
+        */
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits
+          payload: result.hits,
         })
       })
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+      .catch(() =>
+       /* setIsError(true) */
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+      );
   }, [url]);
 
 
@@ -77,6 +137,10 @@ const App = () => {
   }, [handleFetchStories])
   
   const handleRemoveStory = item => {
+    /* 
+    const newStories = stories.filter(story => item.objectID !== story.objectID);
+    setStories(newStories); 
+    */
     dispatchStories({
       type: 'REMOVE_STORY',
       payload: item,
@@ -90,6 +154,15 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
+  /* No longer needed; client-side filtering is implemented
+  const searchedStories = stories.data.filter(story => {
+    const term = searchTerm.toLowerCase();
+    return story.title
+      .toLowerCase()
+      .includes(term);
+  });
+  */
+
   const handleSearchSubmit = () => {
     setUrl(`${API_ENDPOINT}${searchTerm}`)
   };
@@ -97,6 +170,7 @@ const App = () => {
   return (
     <div className="App">
       <h1> My Hacker Stories</h1>
+      {/* <Search search={searchTerm} onSearch={handleSearch}/> */}
       <InputWithLabel 
         id="search" 
         value={searchTerm}
@@ -125,6 +199,18 @@ const App = () => {
   );
 }
 
+/* Refactored to be InputWithLabel
+const Search = ({ search, onSearch }) => {
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={search} onChange={onSearch}/>
+    </>
+  );
+
+};
+*/
+
 const InputWithLabel = ({ id, value, type='text', onInputChange, children, isFocused }) => {
   const inputRef = useRef(); // ref object is a persistent value which stays intact over the lifetime of a React component
 
@@ -141,6 +227,17 @@ const InputWithLabel = ({ id, value, type='text', onInputChange, children, isFoc
 
 };
 
+
+/* Using spread and rest operator
+const List = ({ list }) => {
+  return (
+    list.map(({ objectID, ...item }) => { // rest operator
+      return (<Item key={objectID} item={item} />) // spread operator
+    }));
+};
+
+const Item = ({ title, url, author, num_comments, points }) => ( ... );
+*/
 
 const List = ({ list, onRemoveItem }) => {
   return (
