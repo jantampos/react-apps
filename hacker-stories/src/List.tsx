45fbd76750b6f4eae7ab3b58b6363ec0ee6704ea
jantampos <janaubrey.tampos@gmail.com>
2023-07-25 11:29:48 +0100

    Sorting

--- hacker-stories/src/List.tsx
+++ hacker-stories/src/List.tsx
@@ -1,37 +1,80 @@
-import { memo } from 'react';
-import React from 'react';
+import React, { memo, useState } from 'react';
 
 import './App.css';
 import styles from './App.module.css';
+import { ListProps, ItemProps } from './TypesAndInterfaces';
 
-/** Types */
-type Story = {
-  objectID: string;
-  url: string; 
-  title: string;
-  author: string;
-  num_comments: number;
-  points: number;
+import { sortBy } from 'lodash';
+
+/** Constants */
+/* Non working sort Function; have to revisit
+const SORTS: Record<string, Function> = {
+  NONE: (list: Stories) => list, 
+  TITLE: (list: Stories) => sortBy(list, 'title'),
+  AUTHOR: (list: Stories) => sortBy(list, 'author'),
+  COMMENT: (list: Stories) => sortBy(list, 'num_comments').reverse(),
+  POINT: (list: Stories) => sortBy(list, 'points').reverse(),
+}
+
+// https://www.totaltypescript.com/concepts/type-string-cannot-be-used-to-index-type
+const sortFunction = (sort: string, list: Stories) => {
+  return SORTS[sort];
 };
-
-type Stories = Array<Story>;
-
-type ItemProps = {
-  item: Story;
-  onRemoveItem: (item: Story) => void;
-};
-
-type ListProps = {
-  list: Stories;
-  onRemoveItem: (item: Story) => void;
-};
+*/
 
 const List = memo(({ list, onRemoveItem } : ListProps) => {
+  const [sortedList, setSortedList] = useState(list);
+
+  const handleSort = (sort: string) => {
+    console.log('handleSort');
+    let sorted;
+    switch(sort) {
+      case 'TITLE':
+        sorted = sortBy(list, 'title');
+        setSortedList(sorted);
+        break;
+      case 'AUTHOR':
+        sorted = sortBy(list, 'author');
+        setSortedList(sorted);
+        break;
+      case 'COMMENTS':
+        sorted = sortBy(list, 'num_comments');
+        setSortedList(sorted);
+        break;
+      case 'POINTS':
+          sorted = sortBy(list, 'points');
+          setSortedList(sorted);
+          break;
+      default: 
+        setSortedList(list);
+    }
+  };
+
   console.log('List')
   return (
-    list.map(item => { 
-      return (<Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />) 
-    }));
+    <div>
+      <div style={{ display: 'flex', fontWeight: 'bold' }}>
+        <span style={{ width: '40%' }}>
+          <button type="button" onClick={() => handleSort('TITLE')}>Title</button>
+        </span>
+        <span style={{ width: '30%' }}>
+          <button type="button" onClick={() => handleSort('AUTHOR')}>Author</button>
+        </span>
+        <span style={{ width: '10%' }}>
+          <button type="button" onClick={() => handleSort('COMMENTS')}>Comments</button>
+        </span>
+        <span style={{ width: '10%' }}>
+          <button type="button" onClick={() => handleSort('POINTS')}>Points</button>
+        </span>
+        <span style={{ width: '10%' }}>
+          <button type="button" onClick={() => handleSort('ACTIONS')}>Actions</button>
+        </span>
+      </div>
+      { sortedList.map(item => (
+          <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>
+      ))}
+    </div>
+  );
 }); 
 
 const Item = ({ item, onRemoveItem } : ItemProps) => {
